d6059ed1472e64a97c099e2ac02f9d9500753360
Omaenoshi <71465546+Omaenoshi@users.noreply.github.com>
2022-12-02 18:54:49 +0500

    created new scripts

---
+++ M3&TD/Assets/Scripts/Tile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class Tile : MonoBehaviour
+{
+    public int x;
+    public int y;
+
+    public Item item;
+
+    public Image icon;
+
+    public Button button;
+}
