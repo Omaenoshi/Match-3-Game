5c93b9097adeefbb5156e4b9ef9e9fd2dad44086
Omaenoshi <71465546+Omaenoshi@users.noreply.github.com>
2022-12-02 19:01:13 +0500

    added rows init

--- M3&TD/Assets/Scripts/Board.cs
+++ M3&TD/Assets/Scripts/Board.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 public sealed class Board : MonoBehaviour
 {
     public static Board Instance { get; private set; }
-    
+
+    public Row[] rows;
+
+    public Tile[,] Tiles { get; private set; }
+
+    public int Width => Tiles.GetLength(0);
+    public int Height => Tiles.GetLength(1);
+    private void Awake() => Instance = this;
+
+    private void Start()
+    {
+        Tiles = new Tile[rows.Max(row => row.tiles.Length), rows.Length];
+
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                Tiles[x, y] = rows[y].tiles[x];
+            }
+        }
+        
+        
+    }
 }
